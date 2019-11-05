7068050b2a548879b2622c70447dd975b89806a7
Son Roy Almerol <sonroya.almerol@gmail.com>
2019-11-05 19:45:53 +0800

    initial commit

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,131 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class="fit row wrap justify-center items-start">
+    <q-header class="row wrap justify-center items-start">
+      <q-toolbar class="col-7">
+        <q-toolbar-title class="col-2">
+          Rate AdDU Profs
+        </q-toolbar-title>
+        <q-input class="q-ma-sm col-10" rounded dark standout
+          v-model="search" placeholder="Search Professor/Class"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </q-toolbar>
+      <q-tabs
+        v-model="tab"
+        inline-label
+        class="bg-primary text-white col-7"
+      >
+        <q-tab name="home"  label="Home" />
+        <q-tab name="allProfessors"  label="All Professors" />
+        <q-tab name="professorsNeedingReview" label="Professors Needing Review" />
+        <q-tab name="addProfessor" label="Add Professor" />
+        <q-tab name="contactDev" label="Contact Dev" />
+      </q-tabs>
+    </q-header>
+    <q-page-container class="fit row wrap justify-center items-start ripple-background">
+      <router-view class="col-7" />
+      <div class="circle xxlarge shade1"></div>
+      <div class="circle xlarge shade2"></div>
+      <div class="circle large shade3"></div>
+      <div class="circle mediun shade4"></div>
+      <div class="circle small shade5"></div>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MyLayout',
+
+  data() {
+    return {
+      tab: 'home',
+      search: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.circle{
+  position: absolute;
+  border-radius: 50%;
+  background: #003a6c;
+  animation: ripple 15s infinite;
+  box-shadow: 0px 0px 1px 0px #508fb9;
+  z-index: -1;
+}
+
+.small{
+  width: 200px;
+  height: 200px;
+  left: -100px;
+  top: 0px;
+}
+
+.medium{
+  width: 400px;
+  height: 400px;
+  left: -200px;
+  top: -100px;
+}
+
+.large{
+  width: 600px;
+  height: 600px;
+  left: -300px;
+  top: -200px;
+}
+
+.xlarge{
+  width: 800px;
+  height: 800px;
+  left: -400px;
+  top: -300px;
+}
+
+.xxlarge{
+  width: 1000px;
+  height: 1000px;
+  left: -500px;
+  top: -400px;
+}
+
+.shade1{
+  opacity: 0.2;
+}
+.shade2{
+  opacity: 0.5;
+}
+
+.shade3{
+  opacity: 0.7;
+}
+
+.shade4{
+  opacity: 0.8;
+}
+
+.shade5{
+  opacity: 0.9;
+}
+
+@keyframes ripple{
+  0%{
+    transform: scale(0.8);
+  }
+  
+  50%{
+    transform: scale(1.2);
+  }
+  
+  100%{
+    transform: scale(0.8);
+  }
+}
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,68 @@
+<template>
+  <q-page>
+    <q-card style="width: 100%" flat>
+      <q-card-section class="q-ma-lg">
+        <div class="text-h3">
+          Hello Students!
+        </div>
+        <div class="text-subtitle2">
+          Welcome to Rate AdDU Profs, a comprehensive database for knowing your professors
+        </div>
+        <q-input class="q-my-lg col-12" rounded standout
+          v-model="search" placeholder="Search Professor/Class in Ateneo de Davao University"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </q-card-section>
+    </q-card>
+    <q-card class="row" flat>
+      <div class="col-4">
+        <q-card-section>
+          <div class="text-h6">Completely Secure</div>
+          <q-separator />
+        </q-card-section>
+        <q-card-section>
+          We are not going to collect or share your IP address or
+          email to anyone. All reviews are anonymous so do not fear
+          of them knowing who you are!
+        </q-card-section>
+      </div>
+      <div class="col-4">
+        <q-card-section>
+          <div class="text-h6">By AdDU Students, for AdDU Students</div>
+          <q-separator />
+        </q-card-section>
+        <q-card-section>
+          Rate AdDU Profs is created by a student of AdDU as a service
+          to find the best professors in the university. The purpose of
+          this application is for the students to have a better learning
+          experience and give constructive criticisms to their professors.
+        </q-card-section>
+      </div>
+      <div class="col-4">
+        <q-card-section>
+          <div class="text-h6">More Features!</div>
+          <q-separator />
+        </q-card-section>
+        <q-card-section>
+          Rate AdDU Profs is continuously improving and gaining new features.
+          Any ideas are greatly appreciated. Feel free to request more
+          features through my email on the Contact Dev tab.
+        </q-card-section>
+      </div>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      search: '',
+    };
+  },
+};
+</script>
